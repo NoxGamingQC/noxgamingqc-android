cd903edb8489543df394f4e7a2363423e579e854
NoxGamingQC <noxgamingqc@gmail.com>
2023-11-07 23:07:36 -0500

    Update files

--- app/src/main/java/com/example/noxgamingqc/MainActivity.java
+++ app/src/main/java/com/example/noxgamingqc/MainActivity.java
@@ -3,10 +3,15 @@ package com.example.noxgamingqc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Bundle;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
 
     private WebView myWebView;
